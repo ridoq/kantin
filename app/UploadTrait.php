c5e6e84f02503f3384dc482417f549c5a52de202
Sabdo ilahi Dodik <160377298+Dodik357@users.noreply.github.com>
2024-07-02 13:32:26 +0700

    p

--- app/UploadTrait.php
+++ app/UploadTrait.php
@@ -8,12 +8,17 @@ use Illuminate\Support\Facades\Storage;
 trait UploadTrait
 {
 
+    public function exists(string $file) : bool
+    {
+        return Storage::disk('public')->exists($file);
+    }
 
-    public function upload(string $disk, UploadedFile $file, bool $originalName = true)
+    public function upload(string $disk, UploadedFile $file, bool $originalName = false)
         {
+            if(!$this->exists($disk))Storage::makeDirectory($disk);
             if($originalName){
                 return $file->storeAs($disk,$file->getClientOriginalName());
             }
-            return Storage::put('public/'.$disk, $file);
+            return Storage::put($disk, $file);
         }
 }
