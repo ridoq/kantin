f03ac1b3c616fbcc27a56917814859bb8820d08a
ridoq <ridoq444@gmail.com>
2024-07-02 13:05:55 +0700

    nasd

--- app/UploadTrait.php
+++ app/UploadTrait.php
@@ -8,17 +8,12 @@ use Illuminate\Support\Facades\Storage;
 trait UploadTrait
 {
 
-    public function exists(string $file) : bool
-    {
-        return Storage::disk('public')->exists($file);
-    }
 
-    public function upload(string $disk, UploadedFile $file, bool $originalName = false)
+    public function upload(string $disk, UploadedFile $file, bool $originalName = true)
         {
-            if(!$this->exists($disk))Storage::makeDirectory($disk);
             if($originalName){
                 return $file->storeAs($disk,$file->getClientOriginalName());
             }
-            return Storage::put($disk, $file);
+            return Storage::put('public/'.$disk, $file);
         }
 }
