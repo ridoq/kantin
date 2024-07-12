f09177aadcdc10138ed339aa9a7c8bf04e2d0e09
ridoq <ridoq444@gmail.com>
2024-07-12 21:18:23 +0700

    joij

--- app/UploadTrait.php
+++ app/UploadTrait.php
@@ -14,18 +14,12 @@ trait UploadTrait
         return Storage::disk('public')->exists($file);
     }
 
-    public function upload(string $disk, UploadedFile $file, bool $originalName = true)
+    public function upload(string $disk, UploadedFile $file, bool $originalName = false)
     {
         if (!$this->exists($disk)) Storage::makeDirectory($disk);
         if ($originalName) {
             $nama_file = $file->getClientOriginalName();
-            $nama_file_counter = 2;
             $nama_file = Str::slug(pathinfo($nama_file, PATHINFO_FILENAME)) . '.' . $file->getClientOriginalExtension();
-            while (file_exists(storage_path('app/public/gambar/' . $nama_file))) {
-                $nama_file =  $nama_file_counter . "_" . Str::slug(pathinfo($nama_file, PATHINFO_FILENAME)) . '.' . $file->getClientOriginalExtension();
-                $nama_file_counter++;
-            }
-            $nama_file_counter++;
             return $file->storeAs($disk, $nama_file);
         }
         return Storage::put($disk, $file);
